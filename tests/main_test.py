dd7c6d3b8c59c27b5acf909f7f65a70f2617a42d
guillaume-gricourt <guipagui@gmail.com>
2023-01-13 15:56:45 +0100

    tests(neo4jsbml): format_relationships()

--- tests/main_test.py
+++ tests/main_test.py
@@ -6,16 +6,17 @@ class MainTest(unittest.TestCase):
     dataset_path = os.path.join(os.path.dirname(__file__), "dataset")
     # Models
     iml = os.path.join(dataset_path, "iML1515.xml.gz")
+    iml_toy = os.path.join(dataset_path, "iML1515.toy.xml.gz")
     # Pathways
     pathway_one_json = os.path.join(dataset_path, "PathwayModelisation-0.4.1.json")
     # arrows.py - Node
     node_one_dict = dict(
-        id="n21",
+        id="n11",
         labels=["Model"],
         properties=dict(id="str", name="str", metaid="str", sboTerm="str"),
     )
     node_two_dict = dict(
-        id="n2",
+        id="n12",
         position=dict(x=-482.6, y=-158),
         null_one=None,
         null_two="",
@@ -23,4 +24,26 @@ class MainTest(unittest.TestCase):
         properties=dict(
             id="str", metaid="str", name="str", sboTerm="int", spatialDimensions="int"
         ),
+    )
+    node_three_dict = dict(
+        id="n13",
+        labels=["Species"],
+        properties=dict(id="str", name="str", metaid="str", sboTerm="str"),
+    )
+    # arrows.py - Relationship
+    rel_one_dict = dict(
+        id="n21",
+        fromId="n13",
+        toId="n12",
+        type="HAS_COMPARTMENT",
+        properties=dict(),
+        style=dict(),
+    )
+    rel_two_dict = dict(
+        id="n21",
+        fromId="n12",
+        toId="n13",
+        type="IN_COMPARTMENT",
+        properties=dict(),
+        style=dict(),
     )
--- tests/test_sbml.py
+++ tests/test_sbml.py
@@ -1,40 +1,45 @@
 import libsbml
-from tests.main_test import MainTest
-
 from neo4jsbml.arrows import Arrows, Node, Relationship
 from neo4jsbml.sbml import Sbml
+
+from tests.main_test import MainTest
 
 
 class TestSbml(MainTest):
     def setUp(self):
-        self.sbml = Sbml.from_sbml(path=MainTest.iml)
+        self.sbml_iml = Sbml.from_sbml(path=MainTest.iml)
+        self.sbml_toy = Sbml.from_sbml(path=MainTest.iml_toy)
+
+        self.node_two = Node.from_dict(data=MainTest.node_two_dict)
+        self.node_three = Node.from_dict(data=MainTest.node_three_dict)
 
     def test_load(self):
-        self.assertEqual(self.sbml.document.__class__.__name__, "SBMLDocument")
-        self.assertEqual(self.sbml.model.__class__.__name__, "Model")
-        self.assertIs(self.sbml.tag, None)
+        self.assertEqual(self.sbml_iml.document.__class__.__name__, "SBMLDocument")
+        self.assertEqual(self.sbml_iml.model.__class__.__name__, "Model")
+        self.assertIs(self.sbml_iml.tag, None)
         sbml = Sbml.from_sbml(path=MainTest.iml, tag="test")
         self.assertEqual(sbml.tag, "test")
 
     def test_method(self):
-        methods = Sbml.find_method(self.sbml.document, "notes")
+        methods = Sbml.find_method(self.sbml_iml.document, "notes")
         self.assertEqual(methods, ["getNotes"])
-        methods = Sbml.find_method(self.sbml.model, "Id")
+        methods = Sbml.find_method(self.sbml_iml.model, "Id")
         self.assertEqual(methods, ["getId"])
-        methods = Sbml.find_method(self.sbml.model, "Idl")
+        methods = Sbml.find_method(self.sbml_iml.model, "Idl")
         self.assertEqual(methods, ["getAllElementIdList", "getAllElementMetaIdList"])
 
     def test_format_results(self):
-        data = Sbml.format_results(results=MainTest.node_one_dict)
-        self.assertEqual(data, MainTest.node_one_dict)
-        data = Sbml.format_results(results=MainTest.node_two_dict)
-        self.assertNotIn("null_one", data.keys())
-        self.assertNotIn("null_two", data.keys())
-        self.assertEqual(data.keys(), 4)
+        data = Sbml.format_results(results=[MainTest.node_one_dict])
+        self.assertEqual(len(data), 1)
+        self.assertEqual(len(data[0].keys()), len(MainTest.node_one_dict.keys()))
+        data = Sbml.format_results(results=[MainTest.node_two_dict])
+        self.assertNotIn("null_one", data[0].keys())
+        self.assertNotIn("null_two", data[0].keys())
+        self.assertEqual(len(data[0].keys()), 4)
 
     def test_format_nodes(self):
         node_two = Node.from_dict(data=MainTest.node_two_dict)
-        nodes = self.sbml.format_nodes(nodes=[node_two])
+        nodes = self.sbml_iml.format_nodes(nodes=[node_two])
         self.assertEqual(
             nodes,
             [
@@ -58,6 +63,70 @@ class TestSbml(MainTest):
                     "name": "periplasm",
                     "sboTerm": -1,
                     "spatialDimensions": 0,
+                },
+            ],
+        )
+
+    def test_format_relationships_forward(self):
+        nodes = self.sbml_toy.format_nodes(nodes=[self.node_two, self.node_three])
+        # Relationship - forward
+        rel_one = Relationship.from_dict(data=MainTest.rel_one_dict)
+        rels = self.sbml_toy.format_relationships(relationships=[rel_one])
+        self.assertEqual(
+            rels,
+            [
+                {
+                    "left": "Species",
+                    "left_id": "M_octapb_c",
+                    "relationship": "HAS_COMPARTMENT",
+                    "right": "Compartment",
+                    "right_id": "c",
+                },
+                {
+                    "left": "Species",
+                    "left_id": "M_cysi__L_e",
+                    "relationship": "HAS_COMPARTMENT",
+                    "right": "Compartment",
+                    "right_id": "e",
+                },
+                {
+                    "left": "Species",
+                    "left_id": "M_dhap_c",
+                    "relationship": "HAS_COMPARTMENT",
+                    "right": "Compartment",
+                    "right_id": "c",
+                },
+            ],
+        )
+
+    def test_format_relationships_reverse(self):
+        nodes = self.sbml_toy.format_nodes(nodes=[self.node_two, self.node_three])
+        # Relationship - reverse
+        rel_two = Relationship.from_dict(data=MainTest.rel_two_dict)
+        rels = self.sbml_toy.format_relationships(relationships=[rel_two])
+        self.assertEqual(
+            rels,
+            [
+                {
+                    "left": "Compartment",
+                    "left_id": "c",
+                    "relationship": "IN_COMPARTMENT",
+                    "right": "Species",
+                    "right_id": "M_octapb_c",
+                },
+                {
+                    "left": "Compartment",
+                    "left_id": "e",
+                    "relationship": "IN_COMPARTMENT",
+                    "right": "Species",
+                    "right_id": "M_cysi__L_e",
+                },
+                {
+                    "left": "Compartment",
+                    "left_id": "c",
+                    "relationship": "IN_COMPARTMENT",
+                    "right": "Species",
+                    "right_id": "M_dhap_c",
                 },
             ],
         )
