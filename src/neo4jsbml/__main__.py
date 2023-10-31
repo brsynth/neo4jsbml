aaa4b75f845762fd1cfc1a7655d2463ed4f6bb9b
guillaume-gricourt <guipagui@gmail.com>
2023-10-31 11:35:50 +0100

    feat(neo4jsbml): skeleton for  subcmd

--- src/neo4jsbml/__main__.py
+++ src/neo4jsbml/__main__.py
@@ -110,6 +110,88 @@ P_stn_input.add_argument(
 P_stn.set_defaults(func=_cmd_sbml_to_neo4j)
 
 
+def _cmd_sbml_from_neo4j(args):
+    """Create SBML file from Neo4j"""
+    # Check arguments.
+    logging.info("Start - sbml-from-neo4j")
+    if not os.path.isfile(args.input_model_sbml):
+        logging.error("Model SBML file does not exist: %s" % (args.input_model_sbml,))
+        AP.exit(1)
+    if not os.path.isfile(args.input_arrows_json):
+        logging.error(
+            "Modelisation JSON file does not exist: %s" % (args.input_arrows_json,)
+        )
+        AP.exit(1)
+
+    is_dry_run = False
+    if args.parameter_dry_run:
+        logging.info("Dry run mode, no data will be loaded into the database")
+        is_dry_run = True
+
+    # Connection to database
+    logging.info("Connection to database")
+    con = None
+    if args.input_config_ini:
+        if not os.path.isfile(args.input_config_ini):
+            logging.error("File provided does not exist: %s" % (args.input_config_ini,))
+            AP.exit(1)
+        logging.warning("Configuration file is provided, ignore indiviual arguments")
+        con = connect.Connect.from_config(path=args.input_config_ini)
+    elif args.input_auradb_file:
+        if not os.path.isfile(args.input_auradb_txt):
+            logging.error("File provided does not exist: %s" % (args.input_auradb_txt,))
+            AP.exit(1)
+        logging.warning(
+            "Configuration file AuraDB is provided, ignore indiviual arguments"
+        )
+        con = connect.Connect.from_auradb(path=args.input_auradb_txt)
+    else:
+        con = connect.Connect(
+            protocol=args.input_protocol_str,
+            url=args.input_url_str,
+            port=args.input_port_int,
+            user=args.input_user_str,
+            database=args.input_database_str,
+            password_path=args.input_password_txt,
+        )
+    if con.is_connected() is False and is_dry_run is False:
+        logging.error("Unable to connect to the database")
+        AP.exit(1)
+
+    # Load modelisation
+    logging.info("Load modelisation file")
+    arr = arrows.Arrows.from_json(path=args.input_arrows_json)
+
+    # Filter modelisation based on libsbml
+    logging.info("Filter modelisation based on libsbml")
+
+    # Extract entities
+    logging.info("Extract entities")
+
+    # Extract relationships
+    logging.info("Extract relationships")
+
+    # Write model
+    logging.info("Write model")
+
+    logging.info("End - sbml-from-neo4j")
+    return 0
+
+
+P_sfn = AP_subparsers.add_parser("sbml-from-neo4j", help=_cmd_sbml_from_neo4j.__doc__)
+options.add_dbb_connection(parser=P_sfn)
+# Input
+P_sfn_input = P_sfn.add_argument_group("Input")
+options.add_input_modelisation(parser=P_sfn_input)
+# Output
+P_sfn_params = P_sfn.add_argument_group("Parameters")
+P_sfn_params.add_argument(
+    "--output-model-sbml",
+    help="Output the SBML model",
+)
+P_sfn.set_defaults(func=_cmd_sbml_from_neo4j)
+
+
 def _cmd_stats(args):
     """Get statistics: entities and relationships"""
     # Check arguments.
