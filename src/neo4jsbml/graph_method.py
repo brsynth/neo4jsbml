a5c58f921d3200fcddf556d5a0e2913463c1c819
guillaume-gricourt <guipagui@gmail.com>
2024-02-18 19:03:59 +0100

    fix(graph_methods): compare path between graph_methods and arrows_graph

--- src/neo4jsbml/graph_method.py
+++ src/neo4jsbml/graph_method.py
@@ -77,34 +77,67 @@ class GraphMethod(object):
                     ]["properties"]
                     break
         # Label not uniques, compare node with its predecessor both in graph_methods <-> arrows
+        arrows_graph_undirected = arrows_graph.to_undirected()
         label_duplicates = self.select_labels(uniq=False)
+        model_id = self.retrieve_id(prop="labels", value="Model")
         for node_id in self.graph.nodes:
             label = self.graph.nodes[node_id]["labels"]
             # Skip
             if label not in label_duplicates:
                 continue
-            # To flag
-            for arrows_node in arrows_graph.nodes:
+            if nx.has_path(self.graph, source=model_id, target=node_id) is False:
+                continue
+            graph_path = nx.shortest_path(self.graph, source=model_id, target=node_id)
+            graph_path = graph_path[::-1]
+            graph_path_labels = [self.graph.nodes[x]["labels"] for x in graph_path]
+
+            for arrows_node in arrows_graph_undirected.nodes:
                 if GraphMethod.compare_labels(
-                    first=label, second=arrows_graph.nodes[arrows_node]["labels"]
+                    first=label,
+                    second=arrows_graph_undirected.nodes[arrows_node]["labels"],
                 ):
-                    predecessor = None
-                    predecessors = list(self.graph.predecessors(node_id))
-                    if len(predecessors) != 1:
-                        continue
-                    predecessor = predecessors[0]
-                    # General case
-                    for arrows_neighbor in nx.all_neighbors(arrows_graph, arrows_node):
-                        if GraphMethod.compare_labels(
-                            first=self.graph.nodes[predecessor]["labels"],
-                            second=arrows_graph.nodes[arrows_neighbor]["labels"],
+                    predecessors = nx.dfs_predecessors(
+                        arrows_graph_undirected,
+                        source=arrows_node,
+                        depth_limit=len(graph_path),
+                    )
+                    arrows_path = []
+                    for ix, (key, value) in enumerate(predecessors.items()):
+                        if ix < 1:
+                            arrows_path.append(value)
+                        arrows_path.append(key)
+                    arrows_path = arrows_path[: len(graph_path)]
+                    arrows_path_labels = [
+                        arrows_graph_undirected.nodes[x]["labels"][0]
+                        for x in arrows_path
+                    ]
+                    # Check perfect match
+                    is_matched = False
+                    if GraphMethod.compare_labels(
+                        first=graph_path_labels, second=arrows_path_labels
+                    ):
+                        is_matched = True
+                    """
+                    # Partial match, not considering Model entity
+                    else:
+                        short_graph_path_labels = graph_path_labels[:-1]
+                        short_arrows_path_labels = arrows_path_labels[:-1]
+                        if len(
+                            short_graph_path_labels
+                        ) > 1 and GraphMethod.compare_labels(
+                            first=short_graph_path_labels,
+                            second=short_arrows_path_labels,
                         ):
-                            self.graph.nodes[predecessor]["modelisation"] = True
-                            self.graph.nodes[node_id]["modelisation"] = True
-                            self.graph.nodes[node_id][
-                                "properties"
-                            ] = arrows_graph.nodes[arrows_node]["properties"]
-                            break
+                            is_matched = True
+                    """
+                    if is_matched:
+                        for gp_node in graph_path:
+                            self.graph.nodes[gp_node]["modelisation"] = True
+                        # self.graph.nodes[node_id]["modelisation"] = True
+                        self.graph.nodes[node_id]["properties"] = arrows_graph.nodes[
+                            arrows_node
+                        ]["properties"]
+                        break
         # Flag nodes based on relationship's name
         for pair_gm, pair_arrow in itertools.product(
             GraphMethod.generate_pairs(graph=self.graph),
